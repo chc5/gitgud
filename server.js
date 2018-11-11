b7543d29cacb93f08c568f49dc8d2dfff00d9995
chc5 <chc9511@gmail.com>
2018-11-10 21:14:27 -0500

    Set up the file structure for Express

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const morgan = require('morgan');
+const router = require('./routes/routes');
 const app = express();
 const port = process.env.PORT || 5000;
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('combined'));
+
 // API calls
+app.use(router);
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
