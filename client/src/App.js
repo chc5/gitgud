bc8982636227c0caf30d38bcda92e8506e18e019
chc5 <chc9511@gmail.com>
2018-11-10 22:36:49 -0500

    Removed default testing

--- client/src/App.js
+++ client/src/App.js
@@ -10,39 +10,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
+
+	constructor(props) {
     super(props);
     this.history = createHistory();
   }
-  state = {
-    response: '',
-    post: '',
-    responseToPost: '',
-  };
+	
   componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    return body;
-  };
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/api/world', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.post }),
-    });
-    const body = await response.text();
-    this.setState({ responseToPost: body });
-  };
-render() {
+  
+	}
+	
+	render() {
     return (
       <div className="App">
         <Router history={this.history}>
@@ -56,19 +34,6 @@ render() {
             </Switch>
           </div>
         </Router>
-          <p>{this.state.response}</p>
-          <form onSubmit={this.handleSubmit}>
-            <p>
-              <strong>Post to Server:</strong>
-            </p>
-            <input
-              type="text"
-              value={this.state.post}
-              onChange={e => this.setState({ post: e.target.value })}
-            />
-            <button type="submit">Submit</button>
-          </form>
-          <p>{this.state.responseToPost}</p>
       </div>
     );
   }
