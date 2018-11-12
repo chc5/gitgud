c50aa153004b6abc2b2a7361862275e63d5d2dc5
chc5 <chc9511@gmail.com>
2018-11-12 16:35:15 -0500

    Created Login Page

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 
 import TextFieldReducer from './reducers_text_field';
+
 const rootReducer = combineReducers({
   textField: TextFieldReducer,
+
 });
 
 export default rootReducer;
