bd70f9a9ca8965ccd64b49036b8aa5dc1f60b1a0
melvinheks <mcheria000@citymail.cuny.edu>
2018-11-18 18:45:26 -0500

    Doc schema, filter, Date
    taboo words query, Insert into Document collection, fixed spacing and types
    
    Document filter now returns array of tuples, separated taboowords into own schema, added defaults for time
    
    renamed attributes and removed unneeded info
    
    renamed checkdoc to docHandlers, moved gettaboowords outside, corrected types
    
    changed names and fixed types
    
    added doc and taboo schema
    
    fixed method to retrieve taboo indexes

---
+++ server/database/models/taboo.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+mongoose.promise = Promise;
+const Schema = mongoose.Schema;
+
+const TabooWordsSchema = new Schema({
+  word: {type: String, required: true},
+  suggesterId: {type: Schema.Types.ObjectId, ref: 'User'},
+  date: {type: Date, default: Date.now},
+  approverId: {type: Schema.Types.ObjectId, ref: 'User'}
+})
+
+const TabooWordsSchema.statics.getTabooWords = function(cb) {
+  return this.find({}, "word", function(err, results) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, results);
+  }
+};
+const taboo = mongoose.model('TabooWords', TabooWordsSchema);
+module.exports = taboo;
