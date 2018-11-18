3094ff4c4ecffa1fbd8fc19e09e0cc43f6b9a30a
melvinheks <mcheria000@citymail.cuny.edu>
2018-11-18 18:45:26 -0500

    fixed method to retrieve taboo indexes

--- server/database/models/doc.js
+++ server/database/models/doc.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.promise = Promise;
 const Schema = mongoose.Schema;
+const TabooWords = require('./taboo');
 
 const DocSchema = new Schema({
   title: {type: String, required:true},
@@ -11,20 +12,25 @@ const DocSchema = new Schema({
   date_created: {type: Date, default: Data.now}
 });
 
-DocSchema.methods.findAllTabooIdx = function(taboo_words){
+DocSchema.methods.findAllTabooIdx = function(cb){
   //A String is considered a word if there is a space
   //Checks all the words and see if any are in taboo words and replaces those words
   //returns indexes of all taboo words, format at each index: [start, end + 1]
-  idx_list = [];
-  for (let taboo_word of taboo_words){
-    let current_idx = 0;
-    let start_idx = 0;
-    while((start_idx = this.content.indexOf(taboo_word, current_idx)) > -1){
-      idx_list.push([start_idx, start_idx + len(taboo_word)]);
-      current_idx = start_idx + len(taboo_word);  
+  TabooWords.getTabooWords((err, results) => {
+    if (err) {
+      return cb(err);
     }
+    idx_list = [];
+    for (let taboo_word of taboo_words){
+      let current_idx = 0;
+      let start_idx = 0;
+      while((start_idx = this.content.indexOf(taboo_word, current_idx)) > -1){
+        idx_list.push([start_idx, start_idx + len(taboo_word)]);
+        current_idx = start_idx + len(taboo_word);  
+      }
+    }
+    cb(null,idx_list);
   }
-  return idx_list;
 };
 
 const doc = mongoose.model('Document', DocSchema); 
