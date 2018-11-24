620d8a169128cc5aa80c89e76ee0bc9f990c3365
melvinheks <mcheria000@citymail.cuny.edu>
2018-11-24 00:11:44 -0500

    Added revisions schema, revision retrieval and updating upon document update

--- server/controllers/docHandlers.js
+++ server/controllers/docHandlers.js
@@ -1,6 +1,7 @@
 const Doc = require('../database/models/doc');
 const Taboo = require('../database/models/taboo');
 const authentication = ('../authentication');
+const Revision = require('../database/models/revision');
 
 const createDoc = (req, res) => {
   if(!req.isAuthenticated()){
@@ -10,6 +11,7 @@ const createDoc = (req, res) => {
     let docinst = new Doc({
       title: req.body.title,
       content: req.body.content,
+      original_content: req.body.content,
       owner_id: req.user._id,
       locked: false,
     });
@@ -31,7 +33,20 @@ const retrieveDoc = (req, res) => {
       return res.status(404).json({error:"Unable to retrieve your document"});
     }
     if (result) {
-      res.status(200).json({document:result});
+      if (req.body.revisionId) {
+        result.getVersion(req.body.revisionId, function(versionErr, versionResult) {
+          if (versionErr || !versionResult) {
+            return res.status(404).json(versionErr);
+          }
+          if (versionResult) {
+            result.content = versionResult.changes
+            res.status(200).json({document:result});
+          }
+        });
+      }
+      else {
+        res.status(200).json({document:result});
+      }
     }
   });
 };
@@ -52,32 +67,53 @@ const updateDoc = (req, res) => {
     res.redirect('/');
   }
   else {
-    // push entire text field into revisions array
+    // add entire text field into revision collection
+    // push revision id to revisions array
     // TODO : change in future to only store changes
-    Doc.updateOne({_id:req.params.documentId}, { $push: {revisions:req.body.textField}}, function(err, result){
+    let revisionInst = new Revision({
+      doc_id: req.params.documentId,
+      changes: req.body.textField,
+      modifier_id: req.user._id
+    });
+    revisionInst.save(function(err, revision){
       if (err) {
         res.status(500).json({error:"Unable to save this document"});
       }
       else {
-        res.status(200).json({msg:"Document has been saved"});
+        Doc.updateOne({_id:revision.doc_id}, { $push:{revisions:revision._id}, content:req.body.textField}, function(docErr, result){
+          if (docErr) {
+            res.status(500).json({error:"Unable to save this document"});
+            Revision.deleteOne({_id:revision._id});
+          }
+          else {
+            res.status(200).json({msg:"Document has been saved"});
+          }
+        });
       }
     });
   }
 };
+
 const deleteDoc = (req, res) => {
   if(!req.isAuthenticated()){
     res.redirect('/');
   }
   else {
     // TODO: only delete if user owns document?
-    Doc.deleteOne({_id:req.params.documentId}, function(err){
+    Doc.findOneAndDelete({_id:req.params.documentId}, function(err, document){
       if (err) {
         res.status(500).json({error:"Unable to delete this document"});
       }
       else {
+        Revision.deleteMany({_id:{$in:document.revisions}}, function(err){
+          if (err) {
+            console.log("Error deleting revisions");
+          }
+        }); 
         res.status(200).json({msg:"Document has been deleted"});
       }
     });
   }
-}
+};
+
 module.exports = {createDoc, retrieveDoc, getDocList, updateDoc, deleteDoc};
