f57571746aee1b003f993ee202e71b20637bd694
melvinheks <mcheria000@citymail.cuny.edu>
2018-11-25 21:14:40 -0500

    Fixed naming get->retrieve

--- server/routes/docs.js
+++ server/routes/docs.js
@@ -4,7 +4,7 @@ const docControllers = require('../controllers/docHandlers');
 
 router.post('/create', docControllers.createDoc);
 router.post('/retrieve/:documentId', docControllers.retrieveDoc);
-router.post('/retrieveAll', docControllers.getDocList);
+router.post('/retrieveAll', docControllers.retrieveDocList);
 router.post('/update/:documentId', docControllers.updateDoc);
 router.post('/delete/:documentId', docControllers.deleteDoc);
 
