0dd4c03ed9ea494293c7bf4ffb7e85b638632bac
melvinheks <mcheria000@citymail.cuny.edu>
2018-12-10 01:31:21 -0500

    refactored logged in check for requests, separated logic from controllers

--- server/routes/taboo.js
+++ server/routes/taboo.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const tabooControllers = require('../controllers/taboo');
+const authenticationControllers = require('../controllers/authentication');
 
-router.post('/create', tabooControllers.createTaboo);
-router.post('/approve/:tabooId', tabooControllers.approveTaboo);
+router.post('/create', authenticationControllers.checkAuthenticated, tabooControllers.createTaboo);
+router.post('/approve/:tabooId', authenticationControllers.checkAuthenticated, tabooControllers.approveTaboo);
 router.post('/retrieveApproved', tabooControllers.retrieveApprovedTaboo);
 router.post('/retrieveUnapproved', tabooControllers.retrieveUnapprovedTaboo);
-router.post('/delete/:tabooId', tabooControllers.deleteTaboo);
+router.post('/delete/:tabooId', authenticationControllers.checkAuthenticated, tabooControllers.deleteTaboo);
 
 module.exports = router;
