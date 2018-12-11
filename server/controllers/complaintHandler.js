9ac236773be064e2f9bf5bf62bd4bf8cae658f84
melvinheks <mcheria000@citymail.cuny.edu>
2018-12-11 12:23:30 -0500

    added complaints role check

--- server/controllers/complaintHandler.js
+++ server/controllers/complaintHandler.js
@@ -1,150 +1,207 @@
 const DocComplaint = require('../database/models/docComplaint');
 const UserComplaint = require('../database/models/userComplaint');
 const authentication = require('../authentication');
+const Roles = require('./roleCheck');
 
 const createDocComplaint = (req, res) => {
-  let doccomplaintinst = new DocComplaint({
-    content: req.body.text,
-    fromUserId: req.user._id,
-    revisionId: req.body.revisionId,
-    docId: req.body.documentId 
-  });
-  
-  doccomplaintinst.save(function (err, docComplaint) {
-    if (err) {
-      res.status(500).json({error:"Could not create the Document Complaint."});
+  Roles.checkRole(req, {complaint:["doc-create"]}, function(roleErr){
+    if (roleErr) {
+      return res.status(roleErr.status).json({error:roleErr.info});
     }
-    else {
-      console.log("Document Complaint saved to DocComplaints collection.");
-      res.status(200).json({msg:"Document Complaint Created."});
-    }
+    let doccomplaintinst = new DocComplaint({
+      content: req.body.text,
+      fromUserId: req.user._id,
+      revisionId: req.body.revisionId,
+      docId: req.body.documentId
+    });
+
+    doccomplaintinst.save(function (err, docComplaint) {
+      if (err) {
+        res.status(500).json({error:"Could not create the Document Complaint."});
+      }
+      else {
+        console.log("Document Complaint saved to DocComplaints collection.");
+        res.status(200).json({msg:"Document Complaint Created."});
+      }
+    });
   });
 }
 
 const retrieveDocComplaint = (req, res) => {
-  DocComplaint.findOne({_id:req.params.complaintId}).populate("docId", "title").populate("fromUserId", "username").exec(function(err, result){
-    if (err || !result) {
-      res.status(404).json({error:"Could not retrieve the Document Complaint."});
+  Roles.checkRole(req, {complaint:["doc-retrieve"]}, function(roleErr){
+    if (roleErr) {
+      return res.status(roleErr.status).json({error:roleErr.info});
     }
-    else {
-      res.status(200).json({docComplaint:result});
-    }
+    DocComplaint.findOne({_id:req.params.complaintId}).populate("docId", "title").populate("fromUserId", "username").exec(function(err, result){
+      if (err || !result) {
+        res.status(404).json({error:"Could not retrieve the Document Complaint."});
+      }
+      else {
+        res.status(200).json({docComplaint:result});
+      }
+    });
   });
 };
 
 const retrieveDocComplaintList = (req, res) => {
-  DocComplaint.find({}).populate("docId", "title").populate("fromUserId", "username").exec(function(err, results){
-    if (err || !results) {
-      res.status(404).json({error:"Could not retrieve Document Complaints."});
+  Roles.checkRole(req, {complaint:["doc-retrieve"]}, function(roleErr){
+    if (roleErr) {
+      return res.status(roleErr.status).json({error:roleErr.info});
     }
-    else {
-      res.status(200).json({docComplaintList:results});
-    }
+    DocComplaint.find({}).populate("docId", "title").populate("fromUserId", "username").exec(function(err, results){
+      if (err || !results) {
+        res.status(404).json({error:"Could not retrieve Document Complaints."});
+      }
+      else {
+        res.status(200).json({docComplaintList:results});
+      }
+    });
   });
 };
 
 const deleteDocComplaint = (req, res) => {
-  DocComplaint.deleteOne({_id:req.params.complaintId}, function(err){
-    if (err) {
-      res.status(500).json({error:"Could not delete the Document Complaint."});
+  Roles.checkRole(req, {complaint:["doc-delete"]}, function(roleErr){
+    if (roleErr) {
+      return res.status(roleErr.status).json({error:roleErr.info});
     }
-    else {
-      res.status(200).json({msg:"Document Complaint Deleted."});
-    }
+    DocComplaint.deleteOne({_id:req.params.complaintId}, function(err){
+      if (err) {
+        res.status(500).json({error:"Could not delete the Document Complaint."});
+      }
+      else {
+        res.status(200).json({msg:"Document Complaint Deleted."});
+      }
+    });
   });
 }
 
 const retrieveDocComplaintsForDoc = (req, res) => {
-  const docComplaintsQuery = {docId:req.body.documentId};
-  if (!req.body.getProcessed || req.body.getProcessed==="false") {
-    docComplaintsQuery.processed = false;
-  }
-  DocComplaint.find(docComplaintsQuery, function(err, results){
-    if (err || !results) {
-      res.status(404).json({error:"Could not retrieve Document Complaints."});
+  Roles.checkRole(req, {complaint:["doc-retrieve"]}, function(roleErr){
+    if (roleErr) {
+      return res.status(roleErr.status).json({error:roleErr.info});
     }
-    else {
-      res.status(200).json({docComplaintList:results});
+    const docComplaintsQuery = {docId:req.body.documentId};
+    if (!req.body.getProcessed || req.body.getProcessed==="false") {
+      docComplaintsQuery.processed = false;
     }
+    DocComplaint.find(docComplaintsQuery, function(err, results){
+      if (err || !results) {
+        res.status(404).json({error:"Could not retrieve Document Complaints."});
+      }
+      else {
+        res.status(200).json({docComplaintList:results});
+      }
+    });
   });
 }
 
 const processDocComplaint = (req, res) => {
-  DocComplaint.updateOne({_id: req.params.complaintId}, {processed: true}, function(err, result) {
-    if(err){
-      res.status(500).json({error:"Could not process the Document Complaint."});
+  Roles.checkRole(req, {complaint:["doc-process"]}, function(roleErr){
+    if (roleErr) {
+      return res.status(roleErr.status).json({error:roleErr.info});
     }
-    else {
-      console.log("Document Complaint in DocComplaint collection updated.");
-      res.status(200).json({msg:"Document Complaint processed."});
-    }
+    DocComplaint.updateOne({_id: req.params.complaintId}, {processed: true}, function(err, result) {
+      if(err){
+        res.status(500).json({error:"Could not process the Document Complaint."});
+      }
+      else {
+        console.log("Document Complaint in DocComplaint collection updated.");
+        res.status(200).json({msg:"Document Complaint processed."});
+      }
+    });
   });
 }
 
 const createUserComplaint = (req, res) => {
-  let usercomplaintinst = new UserComplaint({
-    content: req.body.text,
-    fromUserId: req.user._id,
-    targetUserId: req.body.targetUserId 
-  });
-  
-  usercomplaintinst.save(function (err, userComplaint) {
-    if (err) {
-      res.status(500).json({error:"Could not create the User Complaint."});
+  Roles.checkRole(req, {complaint:["user-create"]}, function(roleErr){
+    if (roleErr) {
+      return res.status(roleErr.status).json({error:roleErr.info});
     }
-    else {
-      console.log("User Complaint saved to UserComplaints collection.");
-      res.status(200).json({msg:"User Complaint Created."});
-    }
+    let usercomplaintinst = new UserComplaint({
+      content: req.body.text,
+      fromUserId: req.user._id,
+      targetUserId: req.body.targetUserId 
+    });
+    
+    usercomplaintinst.save(function (err, userComplaint) {
+      if (err) {
+        res.status(500).json({error:"Could not create the User Complaint."});
+      }
+      else {
+        console.log("User Complaint saved to UserComplaints collection.");
+        res.status(200).json({msg:"User Complaint Created."});
+      }
+    });
   });
 }
 
 const retrieveUserComplaint = (req, res) => {
-  UserComplaint.findOne({_id:req.params.complaintId}).populate("fromUserId", "username").populate("targetUserId", "username").exec(function(err, result){
-    if (err || !result) {
-      res.status(404).json({error:"Could not retrieve the user complaint."});
+  Roles.checkRole(req, {complaint:["user-retrieve"]}, function(roleErr){
+    if (roleErr) {
+      return res.status(roleErr.status).json({error:roleErr.info});
     }
-    else {
-      res.status(200).json({userComplaint:result});
-    }
+    UserComplaint.findOne({_id:req.params.complaintId}).populate("fromUserId", "username").populate("targetUserId", "username").exec(function(err, result){
+      if (err || !result) {
+        res.status(404).json({error:"Could not retrieve the user complaint."});
+      }
+      else {
+        res.status(200).json({userComplaint:result});
+      }
+    });
   });
 };
 
 const retrieveUserComplaintList = (req, res) => {
-  UserComplaint.find({}).populate("fromUserId", "username").populate("targetUserId", "username").exec(function(err, results){
-    if (err || !results) {
-      res.status(404).json({error:"Could not retrieve User Complaints."});
+  Roles.checkRole(req, {complaint:["user-retrieve"]}, function(roleErr){
+    if (roleErr) {
+      return res.status(roleErr.status).json({error:roleErr.info});
     }
-    else {
-      res.status(200).json({userComplaintList:results});
-    }
+    UserComplaint.find({}).populate("fromUserId", "username").populate("targetUserId", "username").exec(function(err, results){
+      if (err || !results) {
+        res.status(404).json({error:"Could not retrieve User Complaints."});
+      }
+      else {
+        res.status(200).json({userComplaintList:results});
+      }
+    });
   });
 };
 
 const deleteUserComplaint = (req, res) => {
-  UserComplaint.deleteOne({_id:req.params.complaintId}, function(err){
-    if (err) {
-      res.status(500).json({error:"Could not delete the User Complaint."});
+  Roles.checkRole(req, {complaint:["user-delete"]}, function(roleErr){
+    if (roleErr) {
+      return res.status(roleErr.status).json({error:roleErr.info});
     }
-    else {
-      res.status(200).json({msg:"User Complaint Deleted."});
-    }
+    UserComplaint.deleteOne({_id:req.params.complaintId}, function(err){
+      if (err) {
+        res.status(500).json({error:"Could not delete the User Complaint."});
+      }
+      else {
+        res.status(200).json({msg:"User Complaint Deleted."});
+      }
+    });
   });
 }
 
 const processUserComplaint = (req, res) => {
-  UserComplaint.updateOne({_id: req.params.complaintId}, {processed: true}, function(err, result) {
-    if(err){
-      res.status(500).json({error:"Could not process the User Complaint."});
+  Roles.checkRole(req, {complaint:["user-process"]}, function(roleErr){
+    if (roleErr) {
+      return res.status(roleErr.status).json({error:roleErr.info});
     }
-    else {
-      console.log("User Complaint in UserComplaint collection updated.");
-      res.status(200).json({msg:"User Complaint processed."});
-    }
+    UserComplaint.updateOne({_id: req.params.complaintId}, {processed: true}, function(err, result) {
+      if(err){
+        res.status(500).json({error:"Could not process the User Complaint."});
+      }
+      else {
+        console.log("User Complaint in UserComplaint collection updated.");
+        res.status(200).json({msg:"User Complaint processed."});
+      }
+    });
   });
 }
 
 const retrieveCurrentUserComplaints = (req, res) => {
+  // is this function necessary?
   const currUsrComplaintsQuery = {targetUserId:req.user._id};
   if (!req.body.getProcessed || req.body.getProcessed==="false") {
     currUsrComplaintsQuery.processed = false;
@@ -160,6 +217,7 @@ const retrieveCurrentUserComplaints = (req, res) => {
 }
 
 const retrieveCurrentUserSentComplaints = (req, res) => {
+  // is this function necessary?
   const currUsrSentComplaintsQuery = {fromUserId:req.user._id};
   if (!req.body.getProcessed || req.body.getProcessed==="false") {
     currUsrSentComplaintsQuery.processed = false;
--- server/database/models/roleConfig.js
+++ server/database/models/roleConfig.js
@@ -12,13 +12,13 @@ const GU = {
 const OU = JSON.parse(JSON.stringify(GU));
 OU.name = "OU";
 Array.prototype.push.apply(OU.resource.document, ["create", "lock", "unlock", "update", "delete", "search"]);
-Array.prototype.push.apply(OU.resource.complaint, ["user-create", "doc-retrieve"]);
+Array.prototype.push.apply(OU.resource.complaint, ["user-create", "doc-retrieve", "doc-process", "doc-delete"]);
 Array.prototype.push.apply(OU.resource.user, ["search"]);
 
 const SU = JSON.parse(JSON.stringify(OU));
 SU.name = "SU";
 Array.prototype.push.apply(SU.resource.document, ["unlock-master"]);
-Array.prototype.push.apply(SU.resource.complaint, ["user-process"]);
+Array.prototype.push.apply(SU.resource.complaint, ["user-retrieve", "user-process", "user-delete"]);
 Array.prototype.push.apply(SU.resource.taboo, ["approve", "retrieve", "delete"]);
 Array.prototype.push.apply(SU.resource.applications, ["approve", "retrieve", "delete"]);
 
