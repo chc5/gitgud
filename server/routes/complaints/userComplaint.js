52f70bb4ea64e7ff0462c4ed75673799c129836e
melvinheks <mcheria000@citymail.cuny.edu>
2018-12-11 19:11:10 -0500

    added login check for all relevant paths, middleware is now in index instead of each route file

--- server/routes/complaints/userComplaint.js
+++ server/routes/complaints/userComplaint.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const complaintController = require('../../controllers/complaintHandler');
-const authenticationControllers = require('../../controllers/authentication');
 
-router.post('/create', authenticationControllers.checkAuthenticated, complaintController.createUserComplaint);
+router.post('/create', complaintController.createUserComplaint);
 router.post('/retrieve/:complaintId', complaintController.retrieveUserComplaint);
 router.post('/retrieveAll', complaintController.retrieveUserComplaintList);
-router.post('/retrieveForUser', authenticationControllers.checkAuthenticated, complaintController.retrieveCurrentUserComplaints);
-router.post('/retrieveFromUser', authenticationControllers.checkAuthenticated, complaintController.retrieveCurrentUserSentComplaints);
-router.post('/process/:complaintId', authenticationControllers.checkAuthenticated, complaintController.processUserComplaint);
-router.post('/delete/:complaintId', authenticationControllers.checkAuthenticated, complaintController.deleteUserComplaint);
+router.post('/retrieveForUser', complaintController.retrieveCurrentUserComplaints);
+router.post('/retrieveFromUser', complaintController.retrieveCurrentUserSentComplaints);
+router.post('/process/:complaintId', complaintController.processUserComplaint);
+router.post('/delete/:complaintId', complaintController.deleteUserComplaint);
 
 module.exports = router;
